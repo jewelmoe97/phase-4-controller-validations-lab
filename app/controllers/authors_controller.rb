ce06e9bd7acf7c33670c1685580589d021c41071
jewel <snowdecember079@gmail.com>
2023-01-24 23:52:49 -0500

    pass

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,12 +6,14 @@ class AuthorsController < ApplicationController
     render json: author
   end
 
-  def create
-    author = Author.create(author_params)
+  
 
-    render json: author, status: :created
-  end
-
+def create
+  author = Author.create!(author_params)
+  render json: author, status: :created
+rescue ActiveRecord::RecordInvalid => invalid
+  render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+end
   private
   
   def author_params
